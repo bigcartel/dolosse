c1d532a6f8530d9d6d69729ab928f60d81e5023e
Max Justus Spransy <maxjustus@gmail.com>
2022-09-28 14:26:57 -0700

    Add events by table reset benchmark

---
+++ events_by_table_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkEventByTableReset(b *testing.B) {
+	ebt := make(EventsByTable, 0)
+	for i := 0; i < 10; i++ {
+		d := make([]*RowInsertData, 100000)
+
+		for i := 0; i < 100000; i++ {
+			d[i] = RowInsertDataPool.Get().(*RowInsertData)
+		}
+
+		ebt[fmt.Sprintf("s%d", i)] = &d
+	}
+
+	for n := 0; n < b.N; n++ {
+		ebt.Reset()
+	}
+}
